62d801457faf012d0d7a3096a9d607dae02f5e78
mrangel <miguelangelrangel58@gmail.com>
2020-07-21 19:02:36 -0500

    pruebamongo_DB

--- src/prueba.js
+++ src/prueba.js
@@ -1,59 +1,34 @@
 const http = require('http');
 const path = require('path');
 const url = require('url');
-const mongoose = require('mongoose');
 const express = require('express');
+var { graphqlHTTP} = require('express-graphql');
+var { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
 const app = express();
 const iport = express();
-const socketio = require('socket.io');
+
 const bodyparser = require('body-parser');
 const server = http.createServer(app);
+const socketio = require('socket.io')(server, {'transports': ['websocket', 'polling']});
 const io = socketio.listen(server);
-require('./socket.js')(io);
+require('./socket.js')(io,app);
+
+//MODELO DE mongodb
 const Usuario = mongoose.model('users', { 
-	name: String,
-	position:{ x:Number,y:Number}
-	 });
-//PUBLIC KEY uqkyouzx
-//PRIVATE KEY 342c923c-9828-420e-92bd-2cfec87dd7fa
-//smrangel:marc1928MA@host:3000/nombre_BD
-mongoose.connect('mongodb+srv://mrangel:marc1928MA@integracion-nodejs.hqcyo.mongodb.net/dynamicinfo?retryWrites=true&w=majority',
-	{useNewUrlParser: true, useUnifiedTopology: true},()=>{
-		console.log("conexion con DB");
-		Usuario.create({
-			name:"juan",
-			position:{x:14,y:124} 
-		});
-		Usuario.create({
-			name:"pedro",
-			position:{x:14,y:124} 
-		});
-		Usuario.create({
-			name:"rosa",
-			position:{x:14,y:124} 
-		});
-		Usuario.create({
-			name:"Franco",
-			position:{x:14,y:124} 
-		});
-		Usuario.create({
-			name:"Lucas",
-			position:{x:14,y:124} 
-		});
-});
+	name: String,position:{ x:Number,y:Number}});
 
-
-app.set('port', process.env.PORT||3000);
+//MODELO DE GRAPH QL
+const schema = buildSchema(`type Query {hello: String,bye:String}`);
+var root = {
+  hello :'Hello worasdasd!',
+  bye:'adios!'
+};
 app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname,'public')));
-app.post('/viral',function (req, res){
-	// curl http://localhost:3000/viral -d '{"a":12,"b":{"c":12,"d":15},"k":176}' -H 'Content-Type:application/json'//console.log("Se accedio a la pagina 2)");
-	console.log(req.body);//res.writeHead(200);
-	res.json({})
 
-	res.end();
-	//io.emit(req.body);
-});
+app.set('port', process.env.PORT||3000);
+
 	//console.log('///////////////////////////////////////////////////////////////////////');
 	//console.log(io.Server);
 server.listen(app.get('port'),() => {
--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,17 @@
-module.exports =function(io){
+module.exports =function(io,app){
+	app.post('/v',async (req, res) =>{
+	// curl http://localhost:3000/viral -d '{"a":12,"b":{"c":12,"d":15},"k":176}' -H 'Content-Type:application/json'//console.log("Se accedio a la pagina 2)");
+	
+	//const us = await Usuario.find(); 
+	console.log("req.body");//res.writeHead(200);
+	socket.on('send',function(data){
+		console.log('data');
+	});
+	res.end();
+	//io.emit(req.body);
+});
 	io.on('connection', socket =>{	
-	io.emit("");
+	//io.emit("");
 
 	});
 
